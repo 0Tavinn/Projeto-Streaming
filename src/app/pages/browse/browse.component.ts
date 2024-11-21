850530e03858fe5f1d9b07673a022856a55afb09
0Tavinn <luizota32@gmail.com>
2024-11-21 15:02:30 -0300

    Projeto finalizado
    Colocado a ultima parte da estilização para que o projeto fique completo.

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -15,6 +15,19 @@ export class BrowseComponent {
   filmePantera: any; // Variável para armazenar o filme Pantera Negra
   filmeVenom: any; // Variável para armazenar o filme Venom: A Última Rodada
   filmeGLadiadorII: any;
+  filmeDeadPool : any;
+  filmeDuna : any;
+  filmeMoana2 : any;
+  filmeGarfild : any;
+  filmeJohn4 : any;
+  filmeWicked : any;
+  filmeSorria2 : any;
+  filmeMeuMal : any;
+  filmeDiverti : any;
+  filmeOperacao : any;
+  filmeAlien : any;
+  filmeSubst : any
+
 
   IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
   IMAGE_SIZE = "w500"; // Tamanho das imagens
@@ -26,7 +39,20 @@ export class BrowseComponent {
   loadMovies() {
     const idFilmePantera = 284054; // ID do Pantera Negra
     const idFilmeVenom = 912649; // ID do Venom: A Última Rodada
-    const idFilmeGladiadorII = 558449
+    const idFilmeGladiadorII = 558449;
+    const idFilmeDeadPool = 533535;
+    const idFilmeDuna = 228528;
+    const idFilmeMoana2 = 1241982;
+    const idFilmeGarfild = 748783;
+    const idFilmeJohn4 = 603692;
+    const idFilmeWicked = 402431;
+    const idFilmeSorria2 = 1100782;
+    const idFilmeMeuMal = 519182;
+    const idFilmeDiverti = 1022789;
+    const idFilmeOperacao = 845781;
+    const idFilmeAlien = 945961;
+    const idFilmeSubst = 933260
+
 
     // Carregar o Pantera Negra
     this.filmeService.getMovieById(idFilmePantera).subscribe((result: any) => {
@@ -43,6 +69,54 @@ export class BrowseComponent {
     this.filmeService.getMovieById(idFilmeGladiadorII).subscribe((result:any) => {
       console.log(result);
       this.filmeGLadiadorII = result;
+    });
+    this.filmeService.getMovieById(idFilmeDeadPool).subscribe((result:any) => {
+      console.log(result);
+      this.filmeDeadPool = result;
+    });
+    this.filmeService.getMovieById(idFilmeDuna).subscribe((result:any) => {
+      console.log(result);
+      this.filmeDuna = result;
+    });
+    this.filmeService.getMovieById(idFilmeMoana2).subscribe((result:any) => {
+      console.log(result);
+      this.filmeMoana2 = result;
+    });
+    this.filmeService.getMovieById(idFilmeGarfild).subscribe((result:any) => {
+      console.log(result);
+      this.filmeGarfild = result;
+    });
+    this.filmeService.getMovieById(idFilmeJohn4).subscribe((result:any) => {
+      console.log(result);
+      this.filmeJohn4 = result;
+    });
+    this.filmeService.getMovieById(idFilmeWicked).subscribe((result:any) => {
+      console.log(result);
+      this.filmeWicked = result;
+    });
+    this.filmeService.getMovieById(idFilmeSorria2).subscribe((result:any) => {
+      console.log(result);
+      this.filmeSorria2 = result;
+    });
+    this.filmeService.getMovieById(idFilmeMeuMal).subscribe((result:any) => {
+      console.log(result);
+      this.filmeMeuMal = result;
+    });
+    this.filmeService.getMovieById(idFilmeDiverti).subscribe((result:any) => {
+      console.log(result);
+      this.filmeDiverti = result;
+    });
+    this.filmeService.getMovieById(idFilmeOperacao).subscribe((result:any) => {
+      console.log(result);
+      this.filmeOperacao = result;
+    });
+    this.filmeService.getMovieById(idFilmeAlien).subscribe((result:any) => {
+      console.log(result);
+      this.filmeAlien = result;
+    });
+    this.filmeService.getMovieById(idFilmeSubst).subscribe((result:any) => {
+      console.log(result);
+      this.filmeSubst = result;
     })
   }
 
