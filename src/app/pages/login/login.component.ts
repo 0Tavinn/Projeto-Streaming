b283464907276330898dda083b3993cb11fed5ba
0Tavinn <luizota32@gmail.com>
2024-11-28 11:44:00 -0300

    ATT
    Logica do login.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -14,3 +14,31 @@ export class LoginComponent {
     this.router.navigate(['/browse']);
   }
 }
+
+if (typeof document !== 'undefined') {
+  document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    const emailInput = document.getElementById("email") as HTMLInputElement;
+    const passwordInput = document.getElementById("password") as HTMLInputElement;
+
+  
+    form?.addEventListener("submit", (event) => {
+      event.preventDefault();
+  
+      // Validações básicas
+      const email = emailInput.value.trim();
+      const password = passwordInput.value.trim();
+  
+      if (!email) {
+        alert("O email é obrigatório.");
+        return;
+      }
+
+      alert("Cadastro realizado com sucesso!");
+      form.reset(); // Limpa o formulário após o cadastro
+    });
+  }) 
+
+   const form = document.querySelector("form");
+  // Outros acessos ao DOM
+}
